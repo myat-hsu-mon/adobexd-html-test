0ee9d08ba88eb938e385eabffb64196f5779a43c
myat-hsu-mon <myathsumon@legacyfa-asia.com>
2024-05-29 17:52:47 +0700

    format table style

--- src/components/diagnosis/DiagnosisTable.jsx
+++ src/components/diagnosis/DiagnosisTable.jsx
@@ -3,23 +3,25 @@ export default function DiagnosisTable({ diagnosisList }) {
     <table className="w-full table-auto">
       <thead>
         <tr className="body-emphasized-14pt">
-          <th className="bg-color-f6f7f8 rounded-l-xl py-3.5">
+          <th className="bg-color-f6f7f8 rounded-l-xl py-3.5 pl-4 text-left">
             Problem/Diagnosis
           </th>
-          <th className="bg-color-f6f7f8 py-3.5">Description</th>
-          <th className="bg-color-f6f7f8 rounded-r-xl py-3.5">Status</th>
+          <th className="bg-color-f6f7f8 py-3.5 text-left">Description</th>
+          <th className="bg-color-f6f7f8 rounded-r-xl py-3.5 text-left">
+            Status
+          </th>
         </tr>
       </thead>
       <tbody className="divide-y">
         {diagnosisList.map((diagnosis) => (
           <tr key={diagnosis.name}>
-            <td className="body-regular-14 whitespace-nowrap py-5">
+            <td className="body-regular-14 pl-4 whitespace-nowrap py-5 text-left">
               {diagnosis.status}
             </td>
-            <td className="body-regular-14 whitespace-nowrap py-5">
+            <td className="body-regular-14 whitespace-nowrap py-5 text-left">
               {diagnosis.description}
             </td>
-            <td className="body-regular-14 whitespace-nowrap py-5">
+            <td className="body-regular-14 whitespace-nowrap py-5 text-left">
               {diagnosis.name}
             </td>
           </tr>
